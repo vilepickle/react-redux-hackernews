8d2a6bc2957f33f8fd85605178f5e093990166ec
David Lohmeyer <vilepickle@gmail.com>
2018-11-23 12:03:46 -0500

    Add first reducer and store with redux

--- src/index.js
+++ src/index.js
@@ -2,28 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import store from './store';
 import * as serviceWorker from './serviceWorker';
 
-const stories = [
-  {
-    title: 'React',
-    url: 'https://facebook.github.io/react/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://github.com/reactjs/redux',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  }
-];
-
 ReactDOM.render(
-  <App stories={stories} onArchive={() => {}} />, 
+  <App stories={store.getState()} onArchive={() => {}} />, 
   document.getElementById('root')
 );
 
