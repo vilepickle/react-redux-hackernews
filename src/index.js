a8420d379247eb39ee9619c2d9695460ed1d5a4b
David Lohmeyer <vilepickle@gmail.com>
2018-11-23 13:39:19 -0500

    Enable the view to be re-rendered

--- src/index.js
+++ src/index.js
@@ -7,15 +7,15 @@ import { getReadableStories } from './selectors/story';
 import { STORY_ARCHIVE } from './constants/actionTypes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <App
-    stories={getReadableStories(store.getState())}
-    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
-  />, 
-  document.getElementById('root')
-);
+function render() {
+  ReactDOM.render(
+    <App
+      stories={getReadableStories(store.getState())}
+      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+    />, 
+    document.getElementById('root')
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.subscribe(render);
+render();
